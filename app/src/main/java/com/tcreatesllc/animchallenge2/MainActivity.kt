dd7344623f138cfee38f01b1379b8e7e0659407e
Agusioma <terryterrence200@gmail.com>
2024-07-10 13:38:37 +0300

    initialization

---
+++ app/src/main/java/com/tcreatesllc/animchallenge2/MainActivity.kt
@@ -0,0 +1,160 @@
+package com.tcreatesllc.animchallenge2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.tcreatesllc.animchallenge2.ui.theme.AnimChallenge2Theme
+import kotlin.random.Random
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            AnimChallenge2Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AnimatedVolumeLevelBar(modifier = Modifier
+                        .fillMaxWidth()
+                        .height(20.dp))
+                }
+            }
+        }
+    }
+}
+
+
+@Stable
+@Composable
+fun Dp.toPxf(): Float {
+    val density = LocalDensity.current
+    return with(density) { this@toPxf.toPx() }
+}
+
+@Stable
+fun lerpF(start: Float, stop: Float, fraction: Float): Float =
+    (1 - fraction) * start + fraction * stop
+
+@Composable
+fun AnimatedVolumeLevelBar(
+    modifier: Modifier = Modifier,
+    barWidth: Dp = 2.dp,
+    gapWidth: Dp = 2.dp,
+    barColor: Color = Color.LightGray,
+    isAnimating: Boolean = false,
+) {
+    val MaxLinesCount = 100
+    val infiniteAnimation = rememberInfiniteTransition(label = "")
+    val animations = mutableListOf<State<Float>>()
+    val random = remember { Random(System.currentTimeMillis()) }
+
+    repeat(15) {
+        val durationMillis = random.nextInt(750, 2000)
+        animations += infiniteAnimation.animateFloat(
+            initialValue = 0f,
+            targetValue = 1f,
+            animationSpec = infiniteRepeatable(
+                animation = tween(durationMillis),
+                repeatMode = RepeatMode.Reverse,
+            ), label = ""
+        )
+    }
+
+    val barWidthFloat by rememberUpdatedState(newValue = barWidth.toPxf())
+    val gapWidthFloat by rememberUpdatedState(newValue = gapWidth.toPxf())
+
+    val initialMultipliers = remember {
+        mutableListOf<Float>().apply {
+
+            repeat(MaxLinesCount) { this += random.nextFloat() }
+        }
+    }
+
+    val heightDivider by animateFloatAsState(
+        targetValue = if (isAnimating) 1f else 6f,
+        animationSpec = tween(1000, easing = LinearEasing), label = ""
+    )
+
+    Canvas(modifier = modifier) {
+        val canvasHeight = size.height
+        val canvasWidth = size.width
+        val canvasCenterY = canvasHeight / 2f
+
+        val count =
+            (canvasWidth / (barWidthFloat + gapWidthFloat)).toInt().coerceAtMost(MaxLinesCount)
+        val animatedVolumeWidth = count * (barWidthFloat + gapWidthFloat)
+        var startOffset = (canvasWidth - animatedVolumeWidth) / 2
+
+        val barMinHeight = 0f
+        val barMaxHeight = canvasHeight / 2f / heightDivider
+
+        repeat(count) { index ->
+            val currentSize = animations[index % animations.size].value
+            var barHeightPercent = initialMultipliers[index] + currentSize
+            if (barHeightPercent > 1.0f) {
+                val diff = barHeightPercent - 1.0f
+                barHeightPercent = 1.0f - diff
+            }
+            val barHeight = lerpF(barMinHeight, barMaxHeight, barHeightPercent)
+
+            drawLine(
+                color = barColor,
+                start = Offset(startOffset, canvasCenterY - barHeight / 2),
+                end = Offset(startOffset, canvasCenterY + barHeight / 2),
+                strokeWidth = barWidthFloat,
+                cap = StrokeCap.Round,
+            )
+            startOffset += barWidthFloat + gapWidthFloat
+        }
+    }
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AnimChallenge2Theme {
+        Greeting("Android")
+    }
+}
